3c1ea916a856db5950fd0ea3709e5ff69cf00482
Krzysztof Samson <Krzysztof Samson@DESKTOP-A5J46VB>
2016-11-30 22:17:25 +0100

    Add project files.

---
+++ DocumentReader/TrainingDay.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace traininghelper
+{
+    public class TrainingDay
+    {
+        public string Exercise;
+        public string Description;
+    }
+}
